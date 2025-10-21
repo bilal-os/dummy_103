8141c1f2371b74fe38197ec9cb28f168b41b7656
bilal-os <mb561366@gmail.com>
2025-10-22 00:22:31 +0500

    indented

--- tests/dummy-test.py
+++ tests/dummy-test.py
@@ -12,21 +12,22 @@ chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
 
 driver = webdriver.Remote(
-	command_executor=executor_url,
-	options=chrome_options
+    command_executor=executor_url,
+    options=chrome_options
 )
 
 try:
-	driver.get("https://www.example.com")
-	WebDriverWait(driver, 10).until(
-			EC.visibility_of(driver.find_element(By.CSS_SELECTOR, "h1")))
-	assert "Example Domain" in driver.title, "Title does not match expected value."
-	more_info = driver.find_element(By.CSS_SELECTOR, "a")
-	print("Found link with text:", more_info.text)
-	print("Test passed: Example.com loaded successfully and title is as expected.")
+    driver.get("https://www.example.com")
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of(driver.find_element(By.CSS_SELECTOR, "h1"))
+    )
+    assert "Example Domain" in driver.title, "Title does not match expected value."
+    more_info = driver.find_element(By.CSS_SELECTOR, "a")
+    print("Found link with text:", more_info.text)
+    print("Test passed: Example.com loaded successfully and title is as expected.")
 
 except AssertionError as e:
-	print("Test failed:", e)
+    print("Test failed:", e)
 
 finally:
-	driver.quit()
+    driver.quit()
